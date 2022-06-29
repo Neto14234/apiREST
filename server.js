f5f39d89473b40fcbb15c8c7d927c32fbd39f2fd
Antônio Neto <78606154+Neto14234@users.noreply.github.com>
2022-06-29 17:39:59 -0300

    Update Server.js

--- server.js
+++ server.js
@@ -1,48 +1,50 @@
-const express = require('express');
-const app = express()
-const data = require("./data.json")
+const express = require("express");
+const app = express();
+const data = require("./data.json");
 
 app.use(express.json());
 
-
-// Verbos HTTP
-//GET, POST, PUT, DELETE
-
-app.get("/clients", function(req,res) {
-    res.json(data);
-
-});
-app.get("/clients/:id", function(req,res) {
-    const { id } = req.params;
-    const client = data.find(cli => cli.id = id);
-
-    if (!client) return res.status(404).json();
-    res.json(client);
-});
-app.post("/clients", function(req,res) {
-    const {name, email} = req.body;
-
-    const client = data.find(clicli.id = id);
-    res.json({name, email});
-});
-app.put("/clients/:id", function(req,res) {
-    const { id } = req.params;
-
-    if (!client) return res.status(404).json();
-    
-    const { name } = req.body;
-
-    client.name = name;
-
-    res.json(client);
-});
-app.delete("/clients/:id", function(req,res){
-    const { id } = req.params;
-    const clientsFIltered = data.filter(client => client.id != id);
-    
-    res.json(clientsFIltered)
+app.get("/clients", function(req, res) {
+  res.json(data);
 });
 
-app.listen(3000,function(){
-    console.log('server is running');
+app.get("/clients/:id", function(req, res) {
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) return res.status(204).json();
+
+  res.json(client);
+});
+
+app.post("/clients", function(req, res) {
+  const { name, email } = req.body;
+
+  // salvar
+
+  res.json({ name, email });
+});
+
+app.put("/clients/:id", function(req, res) {
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) return res.status(204).json();
+
+  const { name } = req.body;
+
+  client.name = name;
+
+  res.json(client);
+});
+
+app.delete("/clients/:id", function(req, res) {
+  const { id } = req.params;
+  const clientsFiltered = data.filter(client => client.id != id);
+
+  res.json(clientsFiltered);
+});
+
+app.listen(3000, function() {
+  console.log("Server is running");
 });
